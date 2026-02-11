8a1f6c11902f56e8c94335f75ab8253e4e805ac2
Seweryn <sewerynpastuch@gmail.com>
2026-02-11 22:22:40 +0100

    variables

---
+++ 1_variables/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+	public static void main(String[] args){
+	// vars: int double char boolean
+	// references: string array object
+	// assigment
+	int a=21;
+	System.out.println("the age is"+age);
+	// declaration
+	int b;
+	}
+}
